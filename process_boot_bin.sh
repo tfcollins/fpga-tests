62bca004e26dc017c679ec88748ed9d3d4b7e660
Travis F. Collins <travis.collins@analog.com>
2019-09-11 18:04:02 -0700

    Handle spaces
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- process_boot_bin.sh
+++ process_boot_bin.sh
@@ -6,6 +6,6 @@ FILES=$(find ../test/BINS/ -type f -name "*$BOARD*.BIN" -printf "'%f',")
 for bin in $FILES
 do
   echo "Checking $bin"
-  cp $bin BOOT.BIN
+  cp "$bin" BOOT.BIN
   python3 -m pytest -v test_$BOARD.py
 done
